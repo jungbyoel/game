7efb9bd0b015574561e386e0850a0ef3d487fca6
Leyna Belgherbi <Jung@dorians-macbook-pro.home>
2024-05-20 16:20:03 +0200

    add button and timer

--- Clicker/Clicker/ContentView.swift
+++ Clicker/Clicker/ContentView.swift
@@ -9,20 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     @State var score = 0
+    @State var gameIsInProgress = false
     var body: some View {
         VStack {
             Text("Score : \(score)")
                 .padding()
-            Image(systemName: "plus.square")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .onTapGesture {
-                    score = score + 1
-                }
+            if gameIsInProgress == true {
+                Image(systemName: "plus.square")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .onTapGesture {
+                        score = score + 1
+                    }
+            }
             Spacer()
-                .padding()
-            Button("Nouvelle partie") {
-                
-            }.padding()
+            if gameIsInProgress == false {
+                Button("Nouvelle partie") {
+                    score = 0
+                    gameIsInProgress = true
+                    Timer.scheduledTimer(withTimeInterval: 10.0, repeats: false) { (_) in
+                        gameIsInProgress = false
+                    }
+                }.padding()
+            }
         }
         
     }
